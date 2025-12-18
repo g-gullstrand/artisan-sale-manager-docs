61a625c980ac1d8274b86788e9a33343235afb1d
Gustav Gullstrand <gullstrand@gmail.com>
2025-12-18 12:19:10 +0100

    Remove build artifacts and update gitignore

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/gustavgullstrand/Development/woodlanddev/organicmakers/app/public/wp-content/plugins/artisan-sale-manager/website/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/gustavgullstrand/Development/woodlanddev/organicmakers/app/public/wp-content/plugins/artisan-sale-manager/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/gustavgullstrand/Development/woodlanddev/organicmakers/app/public/wp-content/plugins/artisan-sale-manager/website/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/gustavgullstrand/Development/woodlanddev/organicmakers/app/public/wp-content/plugins/artisan-sale-manager/website/src/css/custom.css"),
-];
